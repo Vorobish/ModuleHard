2dc812ed2582a949b2997ee2324257c33fd59e6c
Настя Х <nastysha7069@yandex.ru>
2024-05-12 22:06:06 +0300

    Update module2hard.py

--- module2hard.py
+++ module2hard.py
@@ -7,7 +7,7 @@ for i in range(1, n):
     for j in range(1, n):
         if n % (i+j) == 0 and i < j:
             result += str(i)+str(j)
-print(result)
+print(f'Шифр для числа {n}: {result}')
 
 
 
